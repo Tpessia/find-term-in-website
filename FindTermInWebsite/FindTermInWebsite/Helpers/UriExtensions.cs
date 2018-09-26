939201613a2beee06d711c24d935f3e0b4d748de
Tpessia <thiago.pessia@hotmail.com>
2018-09-26 15:17:39 -0300

    Bug fixes

---
+++ FindTermInWebsite/FindTermInWebsite/Helpers/UriExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FindTermInWebsite.Helpers
+{
+    public static class UriExtensions
+    {
+        public static Uri ClearUrl(this Uri uri) // Remove hash and query string
+        {
+            var url = uri.AbsoluteUri;
+            var indexHash = url.IndexOf('#');
+            var indexQuery = url.IndexOf('?');
+            var subIndex = indexHash == indexQuery ? url.Length : (indexHash > indexQuery ? indexHash : indexQuery);
+
+            url = url.Substring(0, subIndex);
+            var hasExt = new Regex(@"^(www\.|(?:http|ftp)s?:\/\/|[A-Za-z]:\\|\/\/).*(\/.*\..*)$");
+            if (!hasExt.IsMatch(url))
+                url = url.Trim('/') + "/";
+
+            return new Uri(url);
+        }
+
+        public static Uri ResolveUri(this Uri currentUri, string linkUrl)
+        {
+            //var absolute = new Regex(@"^(www\.|(?:http|ftp)s?:\/\/|[A-Za-z]:\\|\/\/).*"); // https://www.thiago.com.br/teste/index.html
+            //var relative1 = new Regex(@"^(?!www\.|(?:http|ftp)s?:\/\/|[A-Za-z]:\\|\/\/)(\/).*"); // /teste/index.html
+            //var relative2 = new Regex(@"^(?!www\.|(?:http|ftp)s?:\/\/|[A-Za-z]:\\|\/\/)(?!\/|\.\.\/).*"); // index.html
+            //var relative3 = new Regex(@"^(?!www\.|(?:http|ftp)s?:\/\/|[A-Za-z]:\\|\/\/)(?!\/|\.\.\/).*"); // ../index.html
+
+            var currentUrl = currentUri.AbsoluteUri;
+            var hasExt = new Regex(@"^(www\.|(?:http|ftp)s?:\/\/|[A-Za-z]:\\|\/\/).*(\/.*\..*)$");
+            if (!hasExt.IsMatch(currentUrl))
+                currentUrl = currentUrl.Trim('/') + "/";
+
+            return new Uri(new Uri(currentUrl), linkUrl);
+        }
+
+        public static bool IsChildUri(this Uri parentUri, string targetUrl)
+        {
+            var linkUri = ResolveUri(parentUri, targetUrl);
+
+            var currentPathArr = parentUri.AbsolutePath.Trim('/').Split('/');
+            var linkPathArr = linkUri.AbsolutePath.Trim('/').Split('/');
+
+            if (currentPathArr.Length > linkPathArr.Length)
+                return false;
+
+            if (String.IsNullOrWhiteSpace(currentPathArr[0]))
+                return true;
+
+            for (int i = 0; i < currentPathArr.Length; i++)
+            {
+                if (currentPathArr[i] != linkPathArr[i])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ FindTermInWebsite/FindTermInWebsite/WebCrawler.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using FindTermInWebsite.Helpers;
+using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
+
+namespace FindTermInWebsite
+{
+    class WebCrawler
+    {
+        public Uri InitialUri { get; set; }
+
+        public WebCrawler(Uri initialUri)
+        {
+            InitialUri = initialUri;
+        }
+
+        public List<Uri> Search(string term, bool useChildConstraint)
+        {
+            var pagesHistory = new List<Uri>();
+            var pagesQueue = new Queue<Uri>();
+            var pagesFound = new List<Uri>();
+
+            pagesQueue.Enqueue(InitialUri);
+
+            while (pagesQueue.Any())
+            {
+                // Create HTML doc
+                var uriTemp = pagesHistory.Any() ? pagesHistory[pagesHistory.Count - 1].ResolveUri(pagesQueue.Dequeue().AbsoluteUri) : pagesQueue.Dequeue();
+
+                Console.WriteLine("Searching at " + uriTemp.AbsoluteUri);
+
+                HtmlWeb web = new HtmlWeb {PreRequest = OnPreRequest};
+                var htmlDoc = web.Load(uriTemp.AbsoluteUri).DocumentNode;
+
+                try
+                {
+                    // Search for term in page
+                    var termRegex = new Regex(term);
+                    if (termRegex.IsMatch(htmlDoc.OuterHtml))
+                        pagesFound.Add(uriTemp);
+
+
+                    // Search for links in page
+                    var links = htmlDoc.QuerySelectorAll("a[href]") // Get all anchors
+                        .Select(n => uriTemp.ResolveUri(n.Attributes["href"].Value).ClearUrl()) // Get anchors uri
+                        .Distinct() // Remove duplicates
+                        .Where(u => u.Host == uriTemp.Host && (!useChildConstraint || InitialUri.IsChildUri(u.AbsoluteUri))) // Select only from same domain and child
+                        .ToList();
+                    links.RemoveAll(u => u.AbsoluteUri == uriTemp.AbsoluteUri || pagesHistory.Any(h => h.AbsoluteUri == u.AbsoluteUri) || pagesQueue.Any(q => q.AbsoluteUri == u.AbsoluteUri)); // Remove already verified
+
+                    foreach (var link in links)
+                        pagesQueue.Enqueue(link);
+
+                    pagesHistory.Add(uriTemp);
+                }
+                catch (NullReferenceException e)
+                {
+                    Console.WriteLine("INVALID FILE TYPE");
+                }
+            }
+
+            return pagesFound;
+        }
+
+        private static bool OnPreRequest(HttpWebRequest request)
+        {
+            request.AllowAutoRedirect = true;
+            return true;
+        }
+    }
+}
